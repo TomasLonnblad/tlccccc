5931cca26d5076a7fa3e90e53d5e3d0b67a24122
TomasLonnblad <tomaslonnblad@gmail.com>
2025-06-18 18:14:28 +0200

    xxxxx

--- next.config.js
+++ next.config.js
@@ -1,14 +1,22 @@
-/** @type {import('next').NextConfig} */
-module.exports = {
+/**
+ * @type {import('next').NextConfig}
+ */
+const isProd = process.env.NODE_ENV = 'production';
+const nextConfig = {
+  basePath: isProd ? '/nextjs-blog-deployment' : '',
+  output: 'export',
+  distDir: 'dist',
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "assets.vercel.com",
-        port: "",
-        pathname: "/image/upload/**",
-        search: "",
-      },
-    ],
-  },
-};
+    unoptimized: true,
+ },
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+ 
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+ 
+  // Optional: Change the output directory `out` -> `dist`
+  // distDir: 'dist',
+}
+ 
+module.exports = nextConfig
